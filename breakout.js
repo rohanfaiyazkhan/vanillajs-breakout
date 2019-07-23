a2eef5875ee12be50fa0a64289cefa378604655f
Rohan Faiyaz Khan <rkhan@relisource.com>
2019-07-23 20:50:05 +0600

    Added paddle physics

--- breakout.js
+++ breakout.js
@@ -1,77 +1,84 @@
-const canvas = document.getElementById('gameCanvas')
-const ctx = canvas.getContext('2d')
-const ballRadius = 10
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const ballRadius = 10;
 
-let speed = 5
-let x = canvas.width / 2
-let y = canvas.height - 30
-let dx = speed
-let dy = -speed
+let speed = 3;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = speed;
+let dy = -speed;
 
-let paddleHeight = 10
-let paddleWidth = 150
-let paddleX = (canvas.width - paddleWidth) / 2
-let rightPressed = false
-let leftPressed = false
+let paddleHeight = 30;
+let paddleWidth = 150;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
 
-const paddleSpeed = 10
+const paddleSpeed = 10;
 
 function drawBall() {
-	ctx.beginPath()
-	ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
-	ctx.fillStyle = '#0095DD'
-	ctx.fill()
-	ctx.closePath()
+	ctx.beginPath();
+	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+	ctx.fillStyle = '#0095DD';
+	ctx.fill();
+	ctx.closePath();
 }
 
 function drawPaddle() {
-	ctx.beginPath()
-	ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight)
-	ctx.fillStyle = '#0095DD'
-	ctx.fill()
-	ctx.closePath()
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = '#0095DD';
+	ctx.fill();
+	ctx.closePath();
 }
 
 function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
-	drawPaddle()
-	drawBall()
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawPaddle();
+	drawBall();
 
 	if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-		dx = -dx
+		dx = -dx;
 	}
 	if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-		dy = -dy
+		dy = -dy;
+	}
+
+	if (x + dx >= paddleX && x + dx < paddleX + paddleWidth && y + dy >= canvas.height - paddleHeight) {
+		dy = -dy;
+		if (x + dx >= (paddleX + paddleWidth) * 0.9 || x + dx <= (paddleX + paddleWidth) * 0.1) {
+			dx = (x + dx - (paddleX - paddleWidth / 2)) / (paddleWidth / 2);
+		}
 	}
 
 	if (rightPressed && paddleX + paddleWidth < canvas.width) {
-		paddleX += paddleSpeed
+		paddleX += paddleSpeed;
 	}
 
 	if (leftPressed && paddleX > 0) {
-		paddleX -= paddleSpeed
+		paddleX -= paddleSpeed;
 	}
 
-	x += dx
-	y += dy
+	x += dx;
+	y += dy;
 }
 
 function keyDownHandler(e) {
 	if (e.key == 'Right' || e.key == 'ArrowRight') {
-		rightPressed = true
+		rightPressed = true;
 	} else if (e.key == 'Left' || e.key == 'ArrowLeft') {
-		leftPressed = true
+		leftPressed = true;
 	}
 }
 
 function keyUpHandler(e) {
 	if (e.key == 'Right' || e.key == 'ArrowRight') {
-		rightPressed = false
+		rightPressed = false;
 	} else if (e.key == 'Left' || e.key == 'ArrowLeft') {
-		leftPressed = false
+		leftPressed = false;
 	}
 }
 
-document.addEventListener('keydown', keyDownHandler, false)
-document.addEventListener('keyup', keyUpHandler, false)
-setInterval(draw, 10)
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+setInterval(draw, 10);
